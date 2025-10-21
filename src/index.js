a11266e826a635846427ddfce188e5a1d9f0748b
wdNaimur <wd.naimur@gmail.com>
2025-10-21 07:35:34 +0600

    fix: color contrast ratio fix and opening time adjust

--- src/index.js
+++ src/index.js
@@ -109,7 +109,7 @@ function Pizza({ pizzaObj }) {
 
 function Footer() {
   const hour = new Date().getHours();
-  const openHour = 12;
+  const openHour = 9;
   const closeHour = 22;
   const isOpen = hour >= openHour && hour <= closeHour;
   return (
